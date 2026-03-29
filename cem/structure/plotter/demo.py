ff8dc81c85e3e0a03ed1f5e0120b80e3a86246ed
Neil Girdhar <mistersheik@gmail.com>
2026-03-29 03:14:57 -0400

    Merge Trainer into Solver

--- cem/structure/plotter/demo.py
+++ cem/structure/plotter/demo.py
@@ -5,22 +5,22 @@ from collections.abc import Sequence
 from typing import Any, ClassVar
 
 from cem.structure.solution import ExecutionPacket, Telemetries
-from cem.structure.solver import Solver, Trainer
+from cem.structure.solver import Solver
 from .plotter import Plotter
 
 
-class Demo[T: Trainer[Any]]:
+class Demo[T: Solver[Any]]:
     name: ClassVar[str]
     title: ClassVar[str]
 
     @classmethod
     @abstractmethod
-    def create_solvers(cls) -> Sequence[Solver[T]]:
+    def create_solvers(cls) -> Sequence[T]:
         raise NotImplementedError
 
     @classmethod
     @abstractmethod
-    def demo_loss(cls, solvers: Sequence[Solver[T]], packet: ExecutionPacket) -> float:
+    def demo_loss(cls, solvers: Sequence[T], packet: ExecutionPacket) -> float:
         """The demo loss is used to optimize hyperparameters.
 
         The default value of zero means that hyperparameters can't be optimized.
--- cem/structure/solver/__init__.py
+++ cem/structure/solver/__init__.py
@@ -1,12 +1,11 @@
-"""Solver and trainer: hyperparameter fields, execution context management, and path utilities."""
+"""Solver: hyperparameter fields, execution context management, and path utilities."""
 
 from .context_manager import jax_is_initialized, solver_context_manager
-from .solver import Solver, Trainer
+from .solver import Solver
 from .hp_field import bool_field, chooser_field, float_field, int_field
 
 __all__ = [
     "Solver",
-    "Trainer",
     "bool_field",
     "chooser_field",
     "float_field",
