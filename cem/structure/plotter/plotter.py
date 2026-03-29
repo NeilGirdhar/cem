7c15c7fcfe4f1d615c9120a2f3ede75775639c93
Neil Girdhar <mistersheik@gmail.com>
2026-03-29 05:41:51 -0400

    Wipe out Plot and Subplot

--- cem/structure/plotter/plotter.py
+++ cem/structure/plotter/plotter.py
@@ -1,20 +1,11 @@
 from __future__ import annotations
 
 from dataclasses import KW_ONLY
-from typing import Any
 
 import equinox as eqx
+from matplotlib.figure import Figure
 
 from cem.structure.solution import InferenceResults, Telemetries, TrainingResults
-from cem.structure.solver import Solver
-
-from .plot import Plot
-
-
-class ExecutedSolverResults(eqx.Module):
-    solver: Solver[Any]
-    training_results: TrainingResults
-    inference_results: InferenceResults
 
 
 class Plotter(eqx.Module):
@@ -23,7 +14,11 @@ class Plotter(eqx.Module):
     title: str = eqx.field(static=True)
 
     def plot(
-        self, plot: Plot, results: tuple[ExecutedSolverResults, ...], solver_index: int
+        self,
+        figure: Figure,
+        training_results: TrainingResults,
+        inference_results: InferenceResults,
+        solver_index: int,
     ) -> None:
         raise NotImplementedError
 
