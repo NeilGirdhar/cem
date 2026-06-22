644d56bf43aec69d2d3a2b6f1488492675db8fdc
Neil Girdhar <mistersheik@gmail.com>
2026-06-22 10:17:40 -0400

    Remove KDE plots

--- cem/structure/plotter/__init__.py
+++ cem/structure/plotter/__init__.py
@@ -2,19 +2,13 @@
 
 from .demo import Demo, Variant
 from .plotter import Plotter
-from .with_clustering import PlotterWithClustering
-from .with_projection import PlotterWithProjection, ProjectionOptions, plot_kde
 from .with_smooth_graph import PlotterWithSmoothGraph, absolute_percentile, smooth_data
 
 __all__ = [
     "Demo",
     "Plotter",
-    "PlotterWithClustering",
-    "PlotterWithProjection",
     "PlotterWithSmoothGraph",
-    "ProjectionOptions",
     "Variant",
     "absolute_percentile",
-    "plot_kde",
     "smooth_data",
 ]
